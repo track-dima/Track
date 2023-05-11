2998a5b628303c1547c7414e45e7f140dc20e040
Daniele Ambrosino <mail@danieleambrosino.it>
2023-05-11 12:35:24 +0200

    chore: setup dependencies and minor refactor

--- app/src/main/java/it/polimi/dima/track/services/FitbitService.kt
+++ app/src/main/java/it/polimi/dima/track/services/FitbitService.kt
@@ -1,35 +1,38 @@
+package it.polimi.dima.track.services
+
 import android.content.Context
-import com.android.volley.toolbox.RequestQueue
+import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
 class FitbitService(context: Context) {
-  private val baseUrl = "https://api.fitbit.com/1/"
-  private val queue: RequestQueue = Volley.newRequestQueue(context)
+    private val baseUrl = "https://api.fitbit.com/1/"
+    private val queue: RequestQueue = Volley.newRequestQueue(context)
 
-  fun getUserInfo(
-    token: String,
-    onSuccess: (JSONObject) -> Unit,
-    onError: (String) -> Unit
-  ) {
-    val url = "${baseUrl}user/-/profile.json"
-    val request = object : JsonObjectRequest(
-      Method.GET,
-      url,
-      null,
-      { response ->
-        onSuccess(response)
-      },
-      { error -> 
-        onError(error.message ?: "Unknown error occurred")
-      }
+    fun getUserInfo(
+        token: String,
+        onSuccess: (JSONObject) -> Unit,
+        onError: (String) -> Unit
     ) {
-      override fun getHeaders(): MutableMap<String, String> {
-        val headers = HashMap<String, String>()
-        headers["Authorization"] = "Bearer $token"
-        return headers
-      }
+        val url = "${baseUrl}user/-/profile.json"
+        val request = object : JsonObjectRequest(
+            Method.GET,
+            url,
+            null,
+            { response ->
+                onSuccess(response)
+            },
+            { error ->
+                onError(error.message ?: "Unknown error occurred")
+            }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Bearer $token"
+                return headers
+            }
+        }
+        queue.add(request)
     }
-    queue.add(request)
-  }
 }
