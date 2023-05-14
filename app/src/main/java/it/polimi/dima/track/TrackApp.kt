e859cfc247b71e8a6065974384780597afc753e3
Fabio Luccioletti <luccioletti.fabio@gmail.com>
2023-05-14 11:36:56 +0200

    app bar for training screen

--- app/src/main/java/it/polimi/dima/track/TrackApp.kt
+++ app/src/main/java/it/polimi/dima/track/TrackApp.kt
@@ -215,11 +215,10 @@ fun NavGraphBuilder.trackGraph(appState: TrackAppState, navigationType: Navigati
         route = "$TRAINING_SCREEN$TRAINING_ID_ARG",
         arguments = listOf(navArgument(TRAINING_ID) { defaultValue = TRAINING_DEFAULT_ID })
     ) {
-        val trainingId = it.arguments?.getString(TRAINING_ID) ?: TRAINING_DEFAULT_ID
         TrainingScreen(
             popUpScreen = { appState.popUp() },
-            trainingId = trainingId,
-            onEditPressed = {  appState.navigate("$EDIT_TRAINING_SCREEN?$TRAINING_ID={${trainingId}}") }
+            trainingId = it.arguments?.getString(TRAINING_ID) ?: TRAINING_DEFAULT_ID,
+            onEditPressed = {  training -> appState.navigate("$EDIT_TRAINING_SCREEN?$TRAINING_ID={${training.id}}") }
         )
     }
 }
--- app/src/main/java/it/polimi/dima/track/screens/training/TrainingScreen.kt
+++ app/src/main/java/it/polimi/dima/track/screens/training/TrainingScreen.kt
@@ -10,24 +10,35 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import it.polimi.dima.track.R
+import it.polimi.dima.track.common.composable.NoTitleToolbar
+import it.polimi.dima.track.model.Training
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrainingScreen(
   popUpScreen: () -> Unit,
   trainingId: String,
-  onEditPressed: () -> Unit,
+  onEditPressed: (Training) -> Unit,
   viewModel: TrainingViewModel = hiltViewModel()
 ) {
   val training by viewModel.training
@@ -37,33 +48,32 @@ fun TrainingScreen(
   }
 
   Column(
+    modifier = Modifier.fillMaxWidth(),
   ) {
-    Row(
-      modifier = Modifier.padding(12.dp, 8.dp).fillMaxWidth(),
-      horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-      Box() {
+    NoTitleToolbar(
+      navigationIcon = {
         FilledTonalIconButton(
+          modifier = Modifier.padding(8.dp, 0.dp),
           onClick = popUpScreen
         ) {
           Icon(Icons.Outlined.Close, contentDescription = stringResource(R.string.close))
         }
       }
-      Row() {
-        FilledTonalIconButton(
-          modifier = Modifier.padding(4.dp, 0.dp),
-          onClick = onEditPressed
-        ) {
-          Icon(Icons.Outlined.Edit, contentDescription = stringResource(R.string.edit_training))
-        }
-        FilledTonalIconButton(
-          modifier = Modifier.padding(4.dp, 0.dp),
-          onClick = { /* doSomething() */ }
-        ) {
-          Icon(Icons.Outlined.MoreVert, contentDescription = stringResource(R.string.settings))
-        }
+    ) {
+      FilledTonalIconButton(
+        modifier = Modifier.padding(4.dp, 0.dp),
+        onClick = { onEditPressed(training) }
+      ) {
+        Icon(Icons.Outlined.Edit, contentDescription = stringResource(R.string.edit_training))
+      }
+      FilledTonalIconButton(
+        modifier = Modifier.padding(4.dp, 0.dp, 8.dp, 0.dp),
+        onClick = { /* doSomething() */ }
+      ) {
+        Icon(Icons.Outlined.MoreVert, contentDescription = stringResource(R.string.settings))
       }
     }
+
     Text(text = training.title, style = MaterialTheme.typography.titleLarge)
   }
 }
