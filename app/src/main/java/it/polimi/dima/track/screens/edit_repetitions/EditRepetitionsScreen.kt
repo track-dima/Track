c1cefbf3cac2207ae01c10d47f7607c9131be472
Fabio Luccioletti <luccioletti.fabio@gmail.com>
2023-08-08 10:18:14 +0200

    feat: visual improvements

--- app/src/main/java/it/polimi/dima/track/screens/edit_repetitions/EditRepetitionsScreen.kt
+++ app/src/main/java/it/polimi/dima/track/screens/edit_repetitions/EditRepetitionsScreen.kt
@@ -13,18 +13,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.DirectionsRun
+import androidx.compose.material.icons.outlined.DragIndicator
+import androidx.compose.material.icons.outlined.Repeat
+import androidx.compose.material.icons.outlined.Timer
+import androidx.compose.material.icons.rounded.DirectionsRun
+import androidx.compose.material.icons.rounded.DirectionsWalk
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledIconButton
@@ -47,6 +53,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -230,6 +238,7 @@ fun EditRepetitionsScreen(
               )
               TrainingStep.Type.REPETITION -> RepetitionsCardContent(
                 trainingStep,
+                showRecover = trainingStep.id != trainingSteps.last().id,
                 onDeleteClick = { _, trainingStep -> viewModel.onDeleteClick(listOf(), trainingStep) },
                 onEditClick = { _, trainingStep ->
                   openEditDialog.value = true
@@ -273,41 +282,35 @@ fun WarmUpCardContent(
   onDeleteClick: (List<String>, TrainingStep) -> Unit,
   onEditClick: (List<String>, TrainingStep) -> Unit
 ) {
-  ElevatedCard(
+  Card(
     modifier = Modifier
       .fieldModifier()
       .fillMaxWidth()
       .height(70.dp),
-    onClick = { onEditClick(listOf(trainingStep.id), trainingStep) }
+    onClick = { onEditClick(listOf(trainingStep.id), trainingStep) },
+    colors = CardDefaults.cardColors(
+      containerColor = MaterialTheme.colorScheme.secondaryContainer
+    )
   ) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
-      horizontalArrangement = Arrangement.SpaceBetween,
-      verticalAlignment = Alignment.CenterVertically
+    StepCardHeader(
+      modifier = Modifier.fillMaxHeight(),
+      stepType = TrainingStep.Type.WARM_UP,
+      onDeleteClick = { onDeleteClick(listOf(trainingStep.id), trainingStep) }
     ) {
       Column (
         modifier = Modifier.padding(8.dp, 0.dp)
       ) {
         Row {
-          Text(text = stringResource(id = R.string.warm_up), fontWeight = FontWeight.Bold)
+          Text(text = stringResource(id = R.string.warm_up))
         }
         Row {
-          if (trainingStep.durationType == TrainingStep.DurationType.TIME)
-            Text(text = secondsToHhMmSs(trainingStep.duration))
-          else
-            Text(text = trainingStep.distance.toString() + trainingStep.distanceUnit)
+          Text(
+            text = if (trainingStep.durationType == TrainingStep.DurationType.TIME)
+              secondsToHhMmSs(trainingStep.duration)
+            else "${trainingStep.distance} ${trainingStep.distanceUnit}",
+            fontWeight = FontWeight.Bold
+          )
         }
-      }
-      IconButton(
-        modifier = Modifier.padding(8.dp),
-        onClick = { onDeleteClick(listOf(trainingStep.id), trainingStep) }
-      ) {
-        Icon(
-          Icons.Outlined.Delete,
-          contentDescription = stringResource(R.string.delete_repetition)
-        )
       }
     }
   }
@@ -327,37 +330,28 @@ fun CoolDownCardContent(
       .height(70.dp),
     onClick = { onEditClick(listOf(trainingStep.id), trainingStep) },
     colors = CardDefaults.cardColors(
-      containerColor = MaterialTheme.colorScheme.tertiary,
+      containerColor = MaterialTheme.colorScheme.secondaryContainer,
     )
   ) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
-      horizontalArrangement = Arrangement.SpaceBetween,
-      verticalAlignment = Alignment.CenterVertically
+    StepCardHeader(
+      modifier = Modifier.fillMaxHeight(),
+      stepType = TrainingStep.Type.COOL_DOWN,
+      onDeleteClick = { onDeleteClick(listOf(trainingStep.id), trainingStep) }
     ) {
       Column(
         modifier = Modifier.padding(8.dp, 0.dp)
       ) {
         Row {
-          Text(text = stringResource(id = R.string.cool_down), fontWeight = FontWeight.Bold)
+          Text(text = stringResource(id = R.string.cool_down))
         }
         Row {
-          if (trainingStep.durationType == TrainingStep.DurationType.TIME)
-            Text(text = secondsToHhMmSs(trainingStep.duration))
-          else
-            Text(text = trainingStep.distance.toString() + trainingStep.distanceUnit)
+          Text(
+            text = if (trainingStep.durationType == TrainingStep.DurationType.TIME)
+              secondsToHhMmSs(trainingStep.duration)
+            else "${trainingStep.distance} ${trainingStep.distanceUnit}",
+            fontWeight = FontWeight.Bold
+          )
         }
-      }
-      IconButton(
-        modifier = Modifier.padding(8.dp),
-        onClick = { onDeleteClick(listOf(trainingStep.id), trainingStep) }
-      ) {
-        Icon(
-          Icons.Outlined.Delete,
-          contentDescription = stringResource(R.string.delete_repetition)
-        )
       }
     }
   }
@@ -367,55 +361,47 @@ fun CoolDownCardContent(
 @Composable
 fun RepetitionsCardContent(
   trainingStep: TrainingStep,
+  showRecover: Boolean,
   onDeleteClick: (List<String>, TrainingStep) -> Unit,
   onEditClick: (List<String>, TrainingStep) -> Unit
 ) {
-  ElevatedCard(
+  Card(
     modifier = Modifier
       .fieldModifier()
       .fillMaxWidth()
       .height(70.dp),
-    onClick = { onEditClick(listOf(trainingStep.id), trainingStep) }
+    onClick = { onEditClick(listOf(trainingStep.id), trainingStep) },
+    colors = CardDefaults.cardColors(
+      containerColor = MaterialTheme.colorScheme.secondaryContainer,
+    )
   ) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
-      horizontalArrangement = Arrangement.SpaceBetween,
-      verticalAlignment = Alignment.CenterVertically
+    StepCardHeader(
+      modifier = Modifier.fillMaxHeight(),
+      stepType = TrainingStep.Type.REPETITION,
+      onDeleteClick = { onDeleteClick(listOf(trainingStep.id), trainingStep) }
     ) {
-      Column (
+      Column(
         modifier = Modifier.padding(8.dp, 0.dp)
       ) {
-        Row {
-          if (trainingStep.durationType == TrainingStep.DurationType.TIME) {
-            Text(text = "Duration: ", fontWeight = FontWeight.Bold)
-            Text(text = secondsToHhMmSs(trainingStep.duration))
-          }
-          else {
-            Text(text = "Distance: ", fontWeight = FontWeight.Bold)
-            Text(text = trainingStep.distance.toString() + trainingStep.distanceUnit)
+        Column {
+          Text(
+            text = if (trainingStep.durationType == TrainingStep.DurationType.TIME)
+              secondsToHhMmSs(trainingStep.duration)
+            else "${trainingStep.distance} ${trainingStep.distanceUnit}",
+            fontWeight = FontWeight.Bold
+          )
+          if (showRecover) {
+            Row {
+              Text(text = "Recover ")
+              Text(
+                text = if (trainingStep.recoverType == TrainingStep.DurationType.TIME)
+                  secondsToHhMmSs(trainingStep.recoverDuration)
+                else "${trainingStep.recoverDistance}  ${trainingStep.recoverDistanceUnit}",
+                fontWeight = FontWeight.Bold
+              )
+            }
           }
         }
-        Row {
-          if (trainingStep.recoverType == TrainingStep.DurationType.TIME) {
-            Text(text = "Recover time: ", fontWeight = FontWeight.Bold)
-            Text(text = secondsToHhMmSs(trainingStep.recoverDuration))
-          }
-          else {
-            Text(text = "Recover distance: ", fontWeight = FontWeight.Bold)
-            Text(text = trainingStep.recoverDistance.toString() + trainingStep.recoverDistanceUnit)
-          }
-        }
-      }
-      IconButton(
-        modifier = Modifier.padding(8.dp),
-        onClick = { onDeleteClick(listOf(trainingStep.id), trainingStep) }
-      ) {
-        Icon(
-          Icons.Outlined.Delete,
-          contentDescription = stringResource(R.string.delete_repetition)
-        )
       }
     }
   }
@@ -438,16 +424,20 @@ fun RepetitionBlockContent(
     modifier = Modifier
       .fieldModifier()
       .fillMaxWidth()
-      .height(78.dp * (tree.first) + 156.dp * (tree.second)),
+      .height(78.dp * (tree.first) + 156.dp * (tree.second))
   ) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp, 0.dp, 0.dp, 0.dp),
-      verticalAlignment = Alignment.CenterVertically,
-      horizontalArrangement = Arrangement.SpaceBetween,
+    StepCardHeader(
+      stepType = TrainingStep.Type.REPETITION_BLOCK,
+      onDeleteClick = { onDeleteClick(listOf(), repetitionBlock) }
     ) {
-      TextButton(onClick = { onRepetitionsClick(listOf(repetitionBlock.id), repetitionBlock.repetitions) }) {
+      TextButton(
+        onClick = { onRepetitionsClick(listOf(repetitionBlock.id), repetitionBlock.repetitions) }
+      ) {
+        Icon(
+          Icons.Outlined.Repeat,
+          contentDescription = stringResource(R.string.repetitions),
+          modifier = Modifier.padding(end = 4.dp)
+        )
         Text(text = repetitionBlock.repetitions.toString() + " times")
       }
       TextButton(
@@ -455,20 +445,16 @@ fun RepetitionBlockContent(
           onRecoverClick(listOf(repetitionBlock.id), repetitionBlock.recoverType, repetitionBlock.recoverDuration, repetitionBlock.recoverDistance, repetitionBlock.recoverDistanceUnit)
         }
       ) {
+        Icon(
+          Icons.Outlined.Timer,
+          contentDescription = stringResource(R.string.recover),
+          modifier = Modifier.padding(end = 4.dp)
+        )
         Text(
           text = (if (repetitionBlock.recoverType == TrainingStep.DurationType.TIME)
             secondsToHhMmSs(repetitionBlock.recoverDuration)
           else "${repetitionBlock.recoverDistance} ${repetitionBlock.recoverDistanceUnit}")
                   + " in between")
-      }
-      IconButton(
-        modifier = Modifier.padding(8.dp),
-        onClick = { onDeleteClick(listOf(), repetitionBlock) }
-      ) {
-        Icon(
-          Icons.Outlined.Delete,
-          contentDescription = stringResource(R.string.delete_repetition)
-        )
       }
     }
 
@@ -497,6 +483,7 @@ fun RepetitionBlockContent(
             )
             TrainingStep.Type.REPETITION -> RepetitionsCardContent(
               trainingStep,
+              showRecover = trainingStep.id != repetitionBlock.stepsInRepetition.last().id,
               onDeleteClick = { _, trainingStep -> onDeleteClick(listOf(repetitionBlock.id), trainingStep) },
               onEditClick = { _, trainingStep -> onEditClick(listOf(repetitionBlock.id), trainingStep) }
             )
@@ -519,6 +506,134 @@ fun RepetitionBlockContent(
       onAddBlockClick = { _, repetitions -> onAddBlockClick(listOf(repetitionBlock.id), repetitions) }
     )
   }
+}
+
+@Composable
+fun StepCardHeader (
+    modifier: Modifier = Modifier,
+    stepType: String,
+    onDeleteClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+  Row(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(16.dp, 0.dp, 0.dp, 0.dp),
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.SpaceBetween,
+  ) {
+    Row (
+      verticalAlignment = Alignment.CenterVertically
+    ) {
+      Icon(
+        Icons.Outlined.DragIndicator,
+        contentDescription = stringResource(R.string.move_step),
+      )
+
+      when(
+        stepType
+      ) {
+        TrainingStep.Type.WARM_UP -> {
+          Icon(
+            Icons.Rounded.DirectionsWalk,
+            contentDescription = stringResource(R.string.warm_up),
+            modifier = Modifier
+              .padding(horizontal = 8.dp)
+              .drawBehind {
+                drawCircle(
+                  color = Color.Green.copy(alpha = 0.4f),
+                  radius = 16.dp.toPx()
+                )
+              },
+            tint = Color.White
+          )
+        }
+
+        TrainingStep.Type.COOL_DOWN -> {
+          Icon(
+            Icons.Rounded.DirectionsWalk,
+            contentDescription = stringResource(R.string.cool_down),
+            modifier = Modifier
+              .padding(horizontal = 8.dp)
+              .drawBehind {
+                drawCircle(
+                  color = Color.Blue.copy(alpha = 0.4f),
+                  radius = 16.dp.toPx()
+                )
+              },
+            tint = Color.White
+          )
+        }
+
+        TrainingStep.Type.REPETITION -> {
+          Icon(
+            Icons.Rounded.DirectionsRun,
+            contentDescription = stringResource(R.string.repetition),
+            modifier = Modifier
+              .padding(horizontal = 8.dp)
+              .drawBehind {
+                drawCircle(
+                  color = Color.Red.copy(alpha = 0.4f),
+                  radius = 16.dp.toPx()
+                )
+              },
+            tint = Color.White
+          )
+        }
+      }
+
+      content()
+    }
+
+    val openDeleteDialog = rememberSaveable { mutableStateOf(false) }
+
+    IconButton(
+      modifier = Modifier.padding(8.dp),
+      onClick = { openDeleteDialog.value = true }
+    ) {
+      Icon(
+        Icons.Outlined.Delete,
+        contentDescription = stringResource(R.string.delete_repetition)
+      )
+    }
+
+    if (openDeleteDialog.value) {
+      DeleteDialog(
+        onDeleteClick = {
+          openDeleteDialog.value = false
+          onDeleteClick()
+        },
+        onDismissRequest = { openDeleteDialog.value = false }
+      )
+    }
+  }
+
+}
+
+@Composable
+fun DeleteDialog(
+  onDeleteClick: () -> Unit,
+  onDismissRequest: () -> Unit
+) {
+  AlertDialog(
+    onDismissRequest = onDismissRequest,
+    title = { Text(text = stringResource(R.string.delete_repetition)) },
+    text = { Text(text = stringResource(R.string.delete_repetition_confirmation)) },
+    confirmButton = {
+      TextButton(
+        onClick = onDeleteClick
+      ) {
+        Text(text = stringResource(R.string.delete))
+      }
+    },
+    dismissButton = {
+      TextButton(
+        onClick = onDismissRequest
+      ) {
+        Text(text = stringResource(R.string.cancel))
+      }
+    }
+  )
 }
 
 
