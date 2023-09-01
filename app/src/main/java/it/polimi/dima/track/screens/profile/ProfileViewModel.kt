cba5f0d294a58cc5c0905dc9683da0d5bebfc570
Daniele Ambrosino <mail@danieleambrosino.it>
2023-09-01 20:53:33 +0200

    test: ProfileViewModel Unit test

--- app/src/main/java/it/polimi/dima/track/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/it/polimi/dima/track/screens/profile/ProfileViewModel.kt
@@ -2,7 +2,6 @@ package it.polimi.dima.track.screens.profile
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import it.polimi.dima.track.SETTINGS_SCREEN
-import it.polimi.dima.track.model.service.ConfigurationService
 import it.polimi.dima.track.model.service.LogService
 import it.polimi.dima.track.model.service.storage.PersonalBestStorageService
 import it.polimi.dima.track.model.service.storage.TrainingStorageService
@@ -15,8 +14,7 @@ class ProfileViewModel @Inject constructor(
   logService: LogService,
   private val userStorageService: UserStorageService,
   trainingStorageService: TrainingStorageService,
-  personalBestStorageService: PersonalBestStorageService,
-  private val configurationService: ConfigurationService
+  personalBestStorageService: PersonalBestStorageService
 ) : TrackViewModel(logService) {
   val user = userStorageService.user
   val trainings = trainingStorageService.trainings
