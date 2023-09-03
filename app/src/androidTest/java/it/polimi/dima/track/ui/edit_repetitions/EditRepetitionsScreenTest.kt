bed23c67dd065bf0310551f4aef3bfa1501bbc2f
Daniele Ambrosino <mail@danieleambrosino.it>
2023-09-04 00:53:18 +0200

    test: EditRepetitionsScreenTest basic screen test

--- app/src/androidTest/java/it/polimi/dima/track/ui/edit_repetitions/EditRepetitionsScreenTest.kt
+++ app/src/androidTest/java/it/polimi/dima/track/ui/edit_repetitions/EditRepetitionsScreenTest.kt
@@ -3,6 +3,7 @@ package it.polimi.dima.track.ui.edit_repetitions
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import dagger.hilt.android.testing.HiltAndroidTest
 import it.polimi.dima.track.HiltTestActivity
 import it.polimi.dima.track.injection.InjectingTestCase
@@ -16,14 +17,27 @@ class EditRepetitionsScreenTest : InjectingTestCase() {
   val composeTestRule = createAndroidComposeRule<HiltTestActivity>()
 
   @Test
-  fun editRepetitionsScreen() {
+  fun editRepetitionsScreen_Repetitions() {
     composeTestRule.setContent {
       EditRepetitionsScreen(popUpScreen = { })
     }
 
     composeTestRule
+      .onNodeWithText("3x", useUnmergedTree = true)
+      .assertExists()
+      .assertIsDisplayed()
+      .performClick()
+
+    composeTestRule
       .onNodeWithText("Add repetition")
       .assertExists()
       .assertIsDisplayed()
+      .performClick()
+
+    composeTestRule
+      .onNodeWithText("Save")
+      .assertExists()
+      .assertIsDisplayed()
+      .performClick()
   }
 }
