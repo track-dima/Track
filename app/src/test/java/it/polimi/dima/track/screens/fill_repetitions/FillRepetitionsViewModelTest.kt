ecee346c5a3afe0db494904b03a83bd0d75572f3
Daniele Ambrosino <mail@danieleambrosino.it>
2023-09-03 18:25:46 +0200

    test: improve FillRepetitionsViewModelTest coverage

--- app/src/test/java/it/polimi/dima/track/screens/fill_repetitions/FillRepetitionsViewModelTest.kt
+++ app/src/test/java/it/polimi/dima/track/screens/fill_repetitions/FillRepetitionsViewModelTest.kt
@@ -1,10 +1,16 @@
 package it.polimi.dima.track.screens.fill_repetitions
 
 import androidx.lifecycle.SavedStateHandle
+import io.mockk.Awaits
+import io.mockk.Runs
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import it.polimi.dima.track.model.PersonalBest
+import it.polimi.dima.track.model.Training
 import it.polimi.dima.track.model.TrainingStep
 import it.polimi.dima.track.model.service.storage.PersonalBestStorageService
 import it.polimi.dima.track.model.service.storage.TrainingStorageService
@@ -100,11 +106,120 @@ class FillRepetitionsViewModelTest {
   }
 
   @Test
-  fun onDoneClick() = runTest {
+  fun onDoneClick_NewTraining_NoPersonalBest() = runTest {
     val popUpScreen: () -> Unit = mockk()
-    every { popUpScreen() } returns Unit
+    every { popUpScreen() } just Runs
+
+    // Given an already existing training
+    fillRepetitionsViewModel.training.value = Training()
+    coEvery { trainingStorageService.saveTraining(any()) } returns "newTrainingId"
+
+    // And no global personal best
+    coEvery { personalBestStorageService.existsGlobalPersonalBestWithTrainingId(any()) } returns false
+
+
+    coEvery { trainingStorageService.updatePersonalBestFlag(any(), any()) } just Runs
+
+    val step1 = TrainingStep(
+      type = TrainingStep.Type.REPETITION,
+      durationType = TrainingStep.DurationType.DISTANCE,
+      distance = 500,
+      distanceUnit = "m",
+      results = listOf("1:30", "1:40", "1:20")
+    )
+    val step2 = TrainingStep(
+      type = TrainingStep.Type.REPETITION,
+      durationType = TrainingStep.DurationType.DISTANCE,
+      distance = 500,
+      distanceUnit = "m",
+      results = listOf("1:25", "1:45", "1:15")
+    )
+    val step3 = TrainingStep(
+      type = TrainingStep.Type.REPETITION,
+      durationType = TrainingStep.DurationType.DISTANCE,
+      distance = 400,
+      distanceUnit = "m",
+      results = listOf("1:22", "1:42", "1:12")
+    )
+    val step4 = TrainingStep(
+      type = TrainingStep.Type.REPETITION_BLOCK,
+      repetitions = 3,
+      stepsInRepetition = listOf(step1, step2, step3)
+    )
+    fillRepetitionsViewModel.trainingSteps.value = listOf(step4)
+
+    coEvery {
+      personalBestStorageService.getPersonalBestFromDistanceAndTraining(
+        any(),
+        any()
+      )
+    } returns null
+    coEvery {
+      personalBestStorageService.getPersonalBestFromDurationAndTraining(
+        any(),
+        any(),
+      )
+    } returns null
+    coEvery { personalBestStorageService.getGlobalPersonalBestFromDistance(any()) } returns null
+    coEvery { personalBestStorageService.savePersonalBest(any()) } just Awaits
+
     fillRepetitionsViewModel.onDoneClick(popUpScreen)
-    coVerify { popUpScreen() }
+    coVerify { trainingStorageService.saveTraining(any()) }
+  }
+
+  @Test
+  fun onDoneClick_ExistingTraining_WithPersonalBest() = runTest {
+    val popUpScreen: () -> Unit = mockk()
+    every { popUpScreen() } just Runs
+
+    fillRepetitionsViewModel.training.value = Training(id = "trainingId")
+
+    coEvery { trainingStorageService.updateTraining(any()) } just Runs
+    coEvery { personalBestStorageService.existsGlobalPersonalBestWithTrainingId(any()) } returns true
+    coEvery { trainingStorageService.updatePersonalBestFlag(any(), any()) } just Runs
+
+    val step1 = TrainingStep(
+      type = TrainingStep.Type.REPETITION,
+      durationType = TrainingStep.DurationType.DISTANCE,
+      distance = 500,
+      distanceUnit = "m",
+      results = listOf("1:30", "1:40", "1:20")
+    )
+    val step2 = TrainingStep(
+      type = TrainingStep.Type.REPETITION,
+      durationType = TrainingStep.DurationType.DISTANCE,
+      distance = 500,
+      distanceUnit = "m",
+      results = listOf("1:25", "1:45", "1:15")
+    )
+    val step3 = TrainingStep(
+      type = TrainingStep.Type.REPETITION,
+      durationType = TrainingStep.DurationType.DISTANCE,
+      distance = 400,
+      distanceUnit = "m",
+      results = listOf("1:22", "1:42", "1:12")
+    )
+    val step4 = TrainingStep(
+      type = TrainingStep.Type.REPETITION_BLOCK,
+      repetitions = 3,
+      stepsInRepetition = listOf(step1, step2, step3)
+    )
+    fillRepetitionsViewModel.trainingSteps.value = listOf(step4)
+
+    // Personal best
+    coEvery {
+      personalBestStorageService.getPersonalBestFromDistanceAndTraining(
+        any(),
+        any()
+      )
+    } returns PersonalBest(
+      result = "01:23:45",
+      globalPersonalBest = true,
+    )
+    coEvery { personalBestStorageService.getGlobalPersonalBestFromDistance(any()) } returns null
+
+    fillRepetitionsViewModel.onDoneClick(popUpScreen)
+    coVerify { trainingStorageService.updateTraining(any()) }
   }
 
   @Test
