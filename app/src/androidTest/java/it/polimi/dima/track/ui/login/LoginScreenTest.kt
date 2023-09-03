b4b55894943bcbaa3b8edb93adc3d6e4e39f6024
Daniele Ambrosino <mail@danieleambrosino.it>
2023-09-03 20:24:56 +0200

    test: add login/logout flow UI test

--- app/src/androidTest/java/it/polimi/dima/track/ui/login/LoginScreenTest.kt
+++ app/src/androidTest/java/it/polimi/dima/track/ui/login/LoginScreenTest.kt
@@ -3,13 +3,16 @@ package it.polimi.dima.track.ui.login
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidTest
 import it.polimi.dima.track.HiltTestActivity
@@ -36,7 +39,7 @@ class LoginScreenTest : InjectingTestCase() {
   val composeTestRule = createAndroidComposeRule<HiltTestActivity>()
 
   @Test
-  fun loginScreen() {
+  fun loginScreen_AccessibleAndComposed() {
     composeTestRule.setContent {
       TrackApp(
         windowSize = WindowSizeClass.calculateFromSize(size = compactWindowSize),
@@ -75,5 +78,73 @@ class LoginScreenTest : InjectingTestCase() {
       .assertExists()
       .assertIsDisplayed()
       .assertIsEnabled()
+  }
+
+  @Test
+  fun loginScreen_LoginAndLogoutFlow() {
+    composeTestRule.setContent {
+      TrackApp(
+        windowSize = WindowSizeClass.calculateFromSize(size = compactWindowSize),
+        width = compactWindowSize.width
+      )
+    }
+
+    composeTestRule.waitUntilAtLeastOneExists(
+      matcher = hasContentDescription(composeTestRule.activity.getString(R.string.settings)),
+      timeoutMillis = 5_000L
+    )
+
+    composeTestRule.onNodeWithContentDescriptionForStringId(R.string.settings)
+      .assertExists()
+      .performClick()
+
+    composeTestRule.onNodeWithStringId(R.string.sign_in)
+      .assertExists()
+      .performClick()
+
+    composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.email))
+      .assertExists()
+      .assertIsDisplayed()
+      .assertIsEnabled()
+      .performClick()
+      .assertIsFocused()
+      .performTextInput("luccioletti.fabio@gmail.com")
+
+    composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.password))
+      .assertExists()
+      .assertIsDisplayed()
+      .assertIsEnabled()
+      .performClick()
+      .assertIsFocused()
+      .performTextInput("P4ssw0rd")
+
+    composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.sign_in))
+      .assertExists()
+      .assertIsDisplayed()
+      .assertIsEnabled()
+      .assertHasClickAction()
+      .performClick()
+
+    composeTestRule.waitUntilExactlyOneExists(
+      hasContentDescription(composeTestRule.activity.getString(R.string.settings)),
+      timeoutMillis = 5_000L,
+    )
+
+    composeTestRule.onNodeWithContentDescriptionForStringId(R.string.settings)
+      .assertExists()
+      .assertIsDisplayed()
+      .assertHasClickAction()
+      .performClick()
+
+    composeTestRule.waitUntilExactlyOneExists(
+      hasText(composeTestRule.activity.getString(R.string.sign_out)),
+      timeoutMillis = 5_000L,
+    )
+
+    composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.sign_out))
+      .assertExists()
+      .assertIsDisplayed()
+      .assertHasClickAction()
+      .performClick()
   }
 }
